bf3b7a9aac9eb1f3b703a70bafbc3535b4d60f02
spupapi <45003666+spupapi@users.noreply.github.com>
2023-04-19 10:49:30 +0200

    Delete compare_two_list.py

--- compare_two_list.py
+++
@@ -1,9 +0,0 @@
-import re
-
-list1 = ["item1", "item2"]
-list2 = ["item1", "item2"]
-
-for item in list1:
-    for item2 in list2:
-        if re.match(item, item2):
-            print("duplicate: ", item)
